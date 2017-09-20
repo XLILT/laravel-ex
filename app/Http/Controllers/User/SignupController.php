c5dcf841c4ff97b9587186b26baf1253e27dc914
XLILT <xiaolongma002@163.com>
2017-09-20 20:51:26 +0800

    laravel dao

--- app/Http/Controllers/User/SignupController.php
+++ app/Http/Controllers/User/SignupController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\User;
 
+use DB;
 use App\Http\Controllers\Controller;
 
 class SignupController extends Controller
 {
 	public function signup()
 	{
-		return "Hello Control";
+		$users = DB::select('select * from users');
+		return serialize($users);
 	}
 }
 
